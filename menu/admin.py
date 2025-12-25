49a339d680413510a932d7e6b4a83310209f13e6
Axilium1749 <lordragnamauyt@gmail.com>
2025-12-25 22:53:17 +0300

    Menu Models Added

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MenuItem, DayOrder
 
+
+admin.site.register(MenuItem)
+admin.site.register(DayOrder)
 # Register your models here.
--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from models import menu
 
 # Create your views here.
 @login_required
