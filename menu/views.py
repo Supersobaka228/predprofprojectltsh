97f3d1af378e7158d2f0a07203b43e2aa66bc906
Axilium1749 <lordragnamauyt@gmail.com>
2025-12-25 21:23:17 +0300

    technical issues

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from models import menu
 
 # Create your views here.
 @login_required
 def menu(request):
     print(request.user.email)
     return render(request, 'menu/menu.html')
+
+
+
+
+def menu_view(request):
+    today_weekday = datetime.today().isoweekday() # 1-7, пн-вс
+    try:
+        menu
