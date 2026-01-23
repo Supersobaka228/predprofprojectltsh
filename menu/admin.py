3c05b009234ac2c2d38f19291d9313d79ce4f007
Axilium1749 <lordragnamauyt@gmail.com>
2026-01-24 02:43:06 +0300

    new models system worked

--- menu/admin.py
+++ menu/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from .models import MenuItem, DayOrder
+
+from .models import Allergen, DayOrder, Meal, MenuItem
 
 
 @admin.register(DayOrder)
 class DayOrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'day', 'order')
+
+
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'category', 'time', 'price')
+    list_filter = ('category',)
+    search_fields = ('category', 'time')
+    filter_horizontal = ('meals',)
+
+
+@admin.register(Allergen)
+class AllergenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'name', 'sort_order')
+    list_filter = ('sort_order',)
+    search_fields = ('code', 'name')
+    ordering = ('sort_order', 'name')
+
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'weight', 'calories')
+    search_fields = ('name',)
+    filter_horizontal = ('allergens',)
